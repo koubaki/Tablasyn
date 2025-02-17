35886ae85c7b41183cde145bab7464dc42e5f675
koubaki <165947889+koubaki@users.noreply.github.com>
2025-02-17 21:32:26 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -5,15 +5,15 @@ use \Exception;
 
 class Config {
     private $config;
-    
+
     private function init() {
         try {
             if (($config = @file_get_contents(str_replace('/', DIRECTORY_SEPARATOR, __DIR__ . '/config.json'))) === false) {
                 throw new Exception('No config.json file');
             }
-            
+
             $config = json_decode($config, true);
-            
+
             if ((json_last_error() !== JSON_ERROR_NONE) || (!is_array($config))) {
                 throw new Exception('Invalid JSON');
             } else {
@@ -24,15 +24,15 @@ class Config {
             error_log('Error: can\'t get config; PHP error:' . "\n" . $e->getMessage() . "\n", 3, str_replace('/', DIRECTORY_SEPARATOR, __DIR__ . '/logs.txt'));
         }
     }
-    
+
     public function __construct() {
         $this->init();
     }
-    
+
     public function update() {
         $this->init();
     }
-    
+
     public function get() {
         return $this->config;
     }
